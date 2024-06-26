0349c3708202df6621fbc6baa850a7cde676db7a
6666 <1327185450@qq.com>
2024-06-26 21:53:09 +0800

    加载时获取网页的 theme

--- components/extension.tsx
+++ components/extension.tsx
@@ -3,9 +3,44 @@ import {
   CollapsibleContent,
   CollapsibleTrigger
 } from "@/components/ui/collapsible"
+import { useExtionContext } from "context/extension-context"
 import React from "react"
 
 const Extension = () => {
+  const {
+    setExtensionContainer,
+    setExtensionData,
+    setExtensionPanel,
+    setExtensionIsOpen,
+    setExtensionLoading,
+    setExtensionTheme,
+    setExtensionVideoId,
+    extensionContainer,
+    extensionData,
+    extensionIsOpen,
+    extensionLoading,
+    extensionPanel,
+    extensionTheme,
+    extensionVideoId
+  } = useExtionContext()
+
+  React.useEffect(() => {
+    // 获取网页背景颜色
+    const getCssVariables = (name: string) => {
+      const rootStyle = getComputedStyle(document.documentElement)
+
+      return rootStyle.getPropertyValue(name).trim()
+    }
+    const backgroundColor = getCssVariables("--yt-spec-base-background")
+
+    if (backgroundColor === "#fff") {
+      setExtensionTheme("light")
+      return
+    }
+    setExtensionTheme("dark")
+  }, [])
+
+  if (!extensionTheme) return null
   return (
     <div className={`antialiased w-full mb-3 z-10`}>
       <div className="w-full">
