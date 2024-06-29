afd7ca4031905526e1daf9e3febd0ef63fe84d80
6666 <1327185450@qq.com>
2024-06-29 17:38:03 +0800

    粘贴板

--- hooks/use-copy-clipboard.ts
+++ hooks/use-copy-clipboard.ts
@@ -8,7 +8,9 @@ export function useCopyToClipboard({ timeout = 2000 }: CopyToClipboardProps) {
   const [isCopied, setIsCopied] = React.useState(false)
 
   const copyToClipboard = async (value: string) => {
-    if (typeof window !== "undefined" || !navigator.clipboard.writeText) return
+    console.log("value:", value)
+
+    if (typeof window === "undefined" || !navigator.clipboard.writeText) return
     if (!value) return
     await navigator.clipboard.writeText(value)
     setIsCopied(true)
