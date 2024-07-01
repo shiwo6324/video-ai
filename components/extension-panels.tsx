b451f1eb3b9d88d430c7807afba1f9532d3b9f5d
6666 <1327185450@qq.com>
2024-07-02 00:11:00 +0800

    段落

--- components/extension-panels.tsx
+++ components/extension-panels.tsx
@@ -2,13 +2,14 @@ import { useExtionContext } from "context/extension-context"
 import React from "react"
 
 import Summary from "./summary"
+import Transcript from "./transcript"
 
 const ExtensionPanels = () => {
   const { extensionPanel } = useExtionContext()
   return (
     <div>
       {extensionPanel === "总结" && <Summary />}
-      {extensionPanel === "文本" && <h1>文本</h1>}
+      {extensionPanel === "文本" && <Transcript />}
       {extensionPanel === "聊天" && <h1>聊天</h1>}
     </div>
   )
